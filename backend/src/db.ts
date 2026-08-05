8e9ffaaa212c24b37bddba12a14b1025e97a6928
hritika <hritika21013@gmail.com>
2026-08-05 22:21:55 +0530

    Add mongodb

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 const connect=async()=>{
     try{
+          console.log("DATABASE_URL exists:", !!process.env.DATABASE_URL);
         await mongoose.connect(process.env.DATABASE_URL!);
         console.log("Connected to mongodb")
        
