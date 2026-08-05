0338a8b9d1d5d50d9bbbd60175edac09f3e02ddf
hritika <hritika21013@gmail.com>
2026-08-05 22:06:53 +0530

    Add logs

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -2,11 +2,12 @@ import mongoose from "mongoose";
 const connect=async()=>{
     try{
         await mongoose.connect(process.env.DATABASE_URL!);
+        console.log("Connected to mongodb")
        
     }
 
     catch(err){
-        console.error(err)
+        console.error("Mongodb connecttion error",err)
     }
 }
 export default connect;
